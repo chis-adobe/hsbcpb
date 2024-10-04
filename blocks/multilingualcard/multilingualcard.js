d9d228e935eac680ac2224898c4b5f7177c51889
Liviu Chis <chis@adobe.com>
2024-10-04 07:07:15 -0700

    Fixed up multilanguage block

--- blocks/multilingualcard/multilingualcard.js
+++ blocks/multilingualcard/multilingualcard.js
@@ -2,23 +2,30 @@ import { createOptimizedPicture } from "../../scripts/aem.js";
 
 export default function decorate(block) {
   const ul = document.createElement("ul");
-	const languages = {};
+	const languagesDropdown = document.createElement("select");
+	const languages = [];
 
   [...block.children].forEach((row) => {
 		//languages[row.firstElementChild.innerHTML] = 
     const li = document.createElement("li");
     const anchor = document.createElement("a");
     anchor.href = "";
-    const isLink = row.querySelector("div > a");
+    const isLink = row.querySelector("div a");
 
-		const secondElementChild = row.children[1];
+    const languageChild = row.firstElementChild;
+		const language = languageChild.firstElementChild.innerHTML;
+		languages.push(language);
+		row.removeChild(languageChild);
+		
+		// Assign language to li element
+		anchor.className = language;
 
-    while (secondElementChild) {
+		while (row.firstElementChild) {
       if (isLink) {
-        li.append(secondElementChild);
+        li.append(row.firstElementChild);
         anchor.append(li);
       } else {
-        li.append(secondElementChild);
+        li.append(row.firstElementChild);
       }
     }
 
@@ -46,6 +53,28 @@ export default function decorate(block) {
         createOptimizedPicture(img.src, img.alt, false, [{ width: "750" }])
       )
   );
+
+	languages.forEach(language => {
+    const option = document.createElement('option'); // Create a new <option> element
+    option.text = language; // Set the display text
+    option.value = language; // Set the value (optional, could be different from text)
+    languagesDropdown.add(option); // Add the option to the dropdown
+  });
+	
+	languagesDropdown.addEventListener('change', function() {
+		const selectedClass = this.value; // Get the selected value (class name)
+	
+		ul.querySelectorAll("a").forEach(languageRow => {
+			// Check if the <li> has the selected class
+			if (languageRow.classList.contains(selectedClass)) {
+				languageRow.style.display = 'block'; // Show the <li>
+			} else {
+				languageRow.style.display = 'none'; // Hide the <li>
+			}
+		});
+	});
+
   block.textContent = "";
+	block.append(languagesDropdown);
   block.append(ul);
 }
